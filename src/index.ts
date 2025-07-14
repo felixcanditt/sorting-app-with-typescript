4f4c858d2624dcd4e594c2e74733fc0e5854bb52
Felix Canditt <felix.canditt@gmail.com>
2025-07-14 15:47:04 +0200

    added typeguard

--- src/index.ts
+++ src/index.ts
@@ -18,14 +18,20 @@ class Sorter {
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
         // If collection is an array of numbers
-        if (this.collection[j] > this.collection[j + 1]) {
-          const leftHand = this.collection[j];
-          // thats why here, ts error message explains that index signature only permits reading
-          // because in a string, you cant update an index with a new value
-          // so in this case, you cant do it with a number neither because of the union type
-          // union type only allows to use properties both types have, so index with read only in this case
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = leftHand;
+
+        // goal: let ts allow all the methods of a type, not just the ones it has in common with another type in union type
+        // solution: typeguard with instanceof
+        if (this.collection instanceof Array) {
+          // collection === number[]
+          if (this.collection[j] > this.collection[j + 1]) {
+            const leftHand = this.collection[j];
+            // thats why here, ts error message explains that index signature only permits reading
+            // because in a string, you cant update an index with a new value
+            // so in this case, you cant do it with a number neither because of the union type
+            // union type only allows to use properties both types have, so index with read only in this case
+            this.collection[j] = this.collection[j + 1];
+            this.collection[j + 1] = leftHand;
+          }
         }
 
         // if collection is a string
