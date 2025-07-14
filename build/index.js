40daa61a9be30a36ae9f0d023105595bff92e8d2
Felix Canditt <felix.canditt@gmail.com>
2025-07-14 15:36:59 +0200

    added union type to make a difference between string and number[]

--- build/index.js
+++ build/index.js
@@ -6,6 +6,8 @@ class Sorter {
     //   this.collection = collection;
     // }
     // Version 2, refactoring
+    //union type/operator: |
+    // allows to use only features both number[] and string have in common
     constructor(collection) {
         this.collection = collection;
     }
@@ -13,11 +15,17 @@ class Sorter {
         const { length } = this.collection;
         for (let i = 0; i < length; i++) {
             for (let j = 0; j < length - i - 1; j++) {
+                // If collection is an array of numbers
                 if (this.collection[j] > this.collection[j + 1]) {
                     const leftHand = this.collection[j];
+                    // thats why here, ts error message explains that index signature only permits reading
+                    // because in a string, you cant update an index with a new value
+                    // so in this case, you cant do it with a number neither because of the union type
+                    // union type only allows to use properties both types have, so index with read only in this case
                     this.collection[j] = this.collection[j + 1];
                     this.collection[j + 1] = leftHand;
                 }
+                // if collection is a string
             }
         }
     }
