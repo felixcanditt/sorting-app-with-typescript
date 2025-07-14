636a43eaf37842b327c3f94de6dd74a63ec4915e
Felix Canditt <felix.canditt@gmail.com>
2025-07-14 16:22:55 +0200

    created new file Sorter.js

--- build/index.js
+++ build/index.js
@@ -1,29 +1,4 @@
 "use strict";
-class Sorter {
-    constructor(collection) {
-        this.collection = collection;
-    }
-    // why is union type bad here? 
-    // because wed need to update the potential types every time something new is added
-    sort() {
-        const { length } = this.collection;
-        for (let i = 0; i < length; i++) {
-            for (let j = 0; j < length - i - 1; j++) {
-                if (this.collection instanceof Array) {
-                    if (this.collection[j] > this.collection[j + 1]) {
-                        const leftHand = this.collection[j];
-                        this.collection[j] = this.collection[j + 1];
-                        this.collection[j + 1] = leftHand;
-                    }
-                }
-                if (typeof this.collection === 'string') {
-                    this.collection.
-                    ;
-                }
-            }
-        }
-    }
-}
-const newSorter = new Sorter([10, 3, -5, 0]);
-newSorter.sort();
-console.log(newSorter.collection);
+// const newSorter = new Sorter([10, 3, -5, 0]);
+// newSorter.sort();
+// console.log(newSorter.collection);
