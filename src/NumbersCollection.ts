d4668a2483d0ba1df30363146c12685f26bf241c
Felix Canditt <felix.canditt@gmail.com>
2025-07-15 17:12:15 +0200

    added interface

--- src/NumbersCollection.ts
+++ src/NumbersCollection.ts
@@ -1,4 +1,6 @@
-export class NumbersCollection {
+import { Sortable } from './Sorter';
+
+export class NumbersCollection implements Sortable {
   constructor(public myData: number[]) {}
 
   get length(): number {
