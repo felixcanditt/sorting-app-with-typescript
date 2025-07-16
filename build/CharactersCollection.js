03ce4dead17ad5e44d3065a7b1d2a8b1dcf2d079
Felix Canditt <felix.canditt@gmail.com>
2025-07-16 09:52:36 +0200

    added /build to gitignore, changed index.ts

--- build/CharactersCollection.js
+++ build/CharactersCollection.js
@@ -1,1 +1,23 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.CharactersCollection = void 0;
+class CharactersCollection {
+    constructor(myData) {
+        this.myData = myData;
+    }
+    get length() {
+        return this.myData.length;
+    }
+    compare(leftIndex, rightIndex) {
+        return (this.myData[leftIndex].toLowerCase() >
+            this.myData[rightIndex].toLowerCase());
+    }
+    swap(leftIndex, rightIndex) {
+        const characters = this.myData.split('');
+        const leftHand = characters[leftIndex];
+        characters[leftIndex] = characters[rightIndex];
+        characters[rightIndex] = leftHand;
+        this.myData = characters.join('');
+    }
+}
+exports.CharactersCollection = CharactersCollection;
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 
-const numbersCollection = new NumbersCollection([1, 388, -5, 0]);
-const sorter = new Sorter(numbersCollection);
+// const numbersCollection = new NumbersCollection([1, 388, -5, 0]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(1, numbersCollection.myData);
+
+const charactersCollection = new CharactersCollection('Xajgjj');
+const sorter = new Sorter(charactersCollection);
 sorter.sort();
-console.log(1, numbersCollection.myData);
+console.log(charactersCollection.myData);
